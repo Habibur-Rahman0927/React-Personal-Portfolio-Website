e258d0ff017c223885fb7429d34ae178844b16ef
Habibur Rahman <habibur.rahman0927@gmail.com>
2021-06-14 13:13:28 -0700

    added

--- src/Pages/firebase.config.js
+++ src/Pages/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ6-zLxBrSOyMXTszwAGjshVeENJ9uCb0",
-  authDomain: "portfolio-website-15e3a.firebaseapp.com",
-  projectId: "portfolio-website-15e3a",
-  storageBucket: "portfolio-website-15e3a.appspot.com",
-  messagingSenderId: "131275710671",
-  appId: "1:131275710671:web:8eb2232dcaa475570497ba"
+  apiKey: "AIzaSyC0EH_G-ryTVYb0PJ373powu8i0JeZzpac",
+  authDomain: "all-authentication-33695.firebaseapp.com",
+  projectId: "all-authentication-33695",
+  storageBucket: "all-authentication-33695.appspot.com",
+  messagingSenderId: "217711669256",
+  appId: "1:217711669256:web:13c4dd3b60bafd3a0461bd"
 };
 
 export default firebaseConfig;
